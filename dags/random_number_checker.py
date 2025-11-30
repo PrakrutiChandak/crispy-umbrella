ef7deb68c3d70cabb387926cde0ef7f3b1b6e3e2
PrakrutiChandak <prakrutichandak@gmail.com>
2025-11-30 13:52:02 +0530

    adding first random number generator DAG

---
+++ dags/random_number_checker.py
@@ -0,0 +1,28 @@
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+import random
+ 
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    description='A simple DAG to generate and check random numbers',
+    catchup=False,
+    tags='random_number_checker'
+) 
+def random_number_checker():
+    @task
+    def generate_random_number():
+        number = random.randint(1, 100)
+        print(f"Generated random number: {number}")
+        return number
+ 
+    @task 
+    def check_even_odd(number):
+        result = "even" if int(number) % 2 == 0 else "odd"
+        print(f"The number {number} is {result}.")
+    
+    check_even_odd(generate_random_number())
+ 
+random_number_checker()
+
+ 
